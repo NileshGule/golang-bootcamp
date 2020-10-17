d244883ca3d46f2137c0e60fa5827c280e9ea70e
Nilesh Gule <vn_nilesh@yahoo.com>
2020-10-17 21:44:49 +0800

    Added functions to controller

--- controllers/user.go
+++ controllers/user.go
@@ -1,9 +1,12 @@
 package controllers
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
 	"regexp"
+	"strconv"
+
+	"github.com/NileshGule/golang-bootcamp/webservice/models"
 )
 
 type UserController struct {
@@ -11,11 +14,124 @@ type UserController struct {
 }
 
 func (uc UserController) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	_, err := w.Write([]byte("Hello from user controller"))
+
+	if r.URL.Path == "/users" {
+		switch r.Method {
+		case http.MethodGet:
+			uc.getAll(w, r)
+		case http.MethodPost:
+			uc.post(w, r)
+		default:
+			w.WriteHeader(http.StatusNotImplemented)
+		}
+	} else {
+		matches := uc.userIDPattern.FindStringSubmatch(r.URL.Path)
+
+		if len(matches) == 0 {
+			w.WriteHeader(http.StatusNotFound)
+		}
+
+		id, err := strconv.Atoi(matches[1])
+
+		if err != nil {
+			w.WriteHeader(http.StatusNotFound)
+		}
+
+		switch r.Method {
+		case http.MethodGet:
+			uc.get(id, w)
+		case http.MethodPut:
+			uc.put(id, w, r)
+		case http.MethodDelete:
+			uc.delete(id, w)
+		default:
+			w.WriteHeader(http.StatusNotFound)
+		}
+	}
+}
+
+func (uc *UserController) post(w http.ResponseWriter, r *http.Request) {
+	u, err := uc.parseRequest(r)
 
 	if err != nil {
-		fmt.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Could not parse user object"))
+		return
 	}
+
+	u, err = models.AddUser(u)
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	encodeResponseAsJSON(u, w)
+}
+
+func (uc *UserController) put(id int, w http.ResponseWriter, r *http.Request) {
+	u, err := uc.parseRequest(r)
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Could not parse user object"))
+		return
+	}
+
+	if id != u.ID {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("ID of submitted user must match ID in URL"))
+		return
+	}
+
+	u, err = models.UpdateUser(u)
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	encodeResponseAsJSON(u, w)
+}
+
+func (uc *UserController) delete(id int, w http.ResponseWriter) {
+	err := models.RemoveUserById(id)
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+}
+
+func (uc *UserController) parseRequest(r *http.Request) (models.User, error) {
+	dec := json.NewDecoder(r.Body)
+	var u models.User
+	err := dec.Decode(&u)
+
+	if err != nil {
+		return models.User{}, err
+	}
+	return u, nil
+}
+
+func (uc *UserController) getAll(w http.ResponseWriter, r *http.Request) {
+	encodeResponseAsJSON(models.GetUsers(), w)
+}
+
+func (uc *UserController) get(int id, w http.ResponseWriter) {
+	u, err := models.GetUserById(id)
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	encodeResponseAsJSON(u, w)
 }
 
 func NewUserController() *UserController {
